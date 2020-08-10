289f7f1be37527ec30fa4de8e1dd4a793dc09a1c
Soumyason <ddsoumyaraj@gmail.com>
2020-08-10 14:03:29 +0530

    Updates

---
+++ precache-manifest.326ae392e26350112201eeb0caf4b847.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67c9bd17becc657f3494aabb5002a5a",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "bb36d37bb85b114e68f7",
+    "url": "/deploy-react/static/css/main.100c0cd2.chunk.css"
+  },
+  {
+    "revision": "7c4845c2a9bbad9b72b9",
+    "url": "/deploy-react/static/js/2.bbb84c26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.bbb84c26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb36d37bb85b114e68f7",
+    "url": "/deploy-react/static/js/main.6a5629d7.chunk.js"
+  },
+  {
+    "revision": "79ce8f20327857e29de1",
+    "url": "/deploy-react/static/js/runtime-main.f563a091.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
